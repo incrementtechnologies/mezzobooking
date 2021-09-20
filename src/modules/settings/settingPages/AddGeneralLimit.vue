509397cfa3519589d77ab9ad821d4925fe24424b
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-09-20 15:27:45 +0800

    add room limits

--- src/modules/settings/settingPages/AddGeneralLimit.vue
+++ src/modules/settings/settingPages/AddGeneralLimit.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="margin: 56px">
       <span>
-          <span @click="$router.push('/coupons')" class="backBtn">
+          <span @click="$router.push('/general-limit')" class="backBtn">
               <i class="fa fa-chevron-left"></i>
               Back
           </span>
@@ -12,11 +12,14 @@
           </span>
       </span>
       <section class="mt-5">
-        <div class="row">
+        <label class="text-danger">{{errorMessage}}</label>
+        <div class="row mb-3">
           <div class="col-md-6">
             <label>Room Type</label>
             <div class="input-group">
-                <input v-model="type" type="text" class="form-control-custom form-control">
+              <select v-model="type" class="form-control-custom form-control">
+                <option v-for="(type, idx) in types" :key="idx" :value="type.id">{{type.payload_value}}</option>
+              </select>
             </div>
           </div>
           <div class="col-md-6">
@@ -30,13 +33,13 @@
           <div class="col-md-6">
             <label>Start Date</label>
             <div class="input-group">
-                <input v-model="start_date" type="datetime-local" class="form-control-custom form-control">
+                <input v-model="start_date" type="date" class="form-control-custom form-control">
             </div>
           </div>
           <div class="col-md-6">
             <label>End Date</label>
             <div class="input-group">
-                <input v-model="end_date" type="datetime-local" class="form-control-custom form-control">
+                <input v-model="end_date" type="date" class="form-control-custom form-control">
             </div>
           </div>
         </div>
@@ -44,6 +47,55 @@
     </div>
 </template>
 <script>
+import AUTH from 'src/services/auth'
+export default {
+  mounted() {
+    this.retrieveType()
+  },
+  data: () => ({
+    types: [],
+    user: AUTH,
+    type: null,
+    start_date: null,
+    end_date: null,
+    limit: null,
+    errorMessage: null
+  }),
+  methods: {
+    retrieveType(){
+      let parameter = {
+        condition: [
+          {
+            column: 'payload',
+            clause: '=',
+            value: 'room_type'
+          }
+        ]
+      }
+      this.APIRequest('payloads/retrieve', parameter, response => {
+        this.types = response.data
+      })
+    },
+    create(){
+      let parameter = {
+        payload: 'room_type',
+        payload_value: this.type,
+        limit: this.limit,
+        start_date: this.start_date,
+        end_date: this.end_date,
+        status: 'available'
+      }
+      if(Object.keys(parameter).includes('') || Object.keys(parameter).includes(null)){
+        this.errorMessage = 'Field should not be empty'
+        return
+      }
+      $('#loading').css({'display': 'block'})
+      this.APIRequest('availabilities/create', parameter, response => {
+        $('#loading').css({'display': 'none'})
+      })
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
 @import "~assets/style/colors.scss";
