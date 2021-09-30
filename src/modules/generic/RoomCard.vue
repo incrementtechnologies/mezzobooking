3fec1f9d9ba897cea2595cf12db1ebbd96af8b22
chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2021-09-30 08:14:04 +0800

    Updates

--- src/modules/generic/RoomCard.vue
+++ src/modules/generic/RoomCard.vue
@@ -4,47 +4,23 @@
       <div class="card" style="min-height: 240px">
         <div class="card-horizontal">
           <div class="img-square-wrapper">
-            <img class="" style="min-height: 240px; width:240px" src="http://via.placeholder.com/300x180" alt="Card image cap">
+            <img class="" style="min-height: 240px; width:240px" :src="list.images.length > 0 ? config.BACKEND_URL + list.images[0].url : 'http://via.placeholder.com/300x180'" alt="Card image cap">
           </div>
           <div class="card-body">
             <div class="mb-4">
-              <!-- //lacking of room_type -->
               <span style="text-transform: uppercase;"><b>{{list.title}}</b><b style="border-style: ridge; color: gray; margin-left: 1%; font-size: 10px; padding: 3px;">{{list.category.payload_value}}</b></span>
-              <!-- //lacking of currency and price terms -->
-              <span style="float:right"><b>PHP{{list.price !== null ? list.price : 'No indicated price'}}/Night</b><i class="fa fa-pencil ml-2"></i></span>
+              <span style="float:right"><b>{{list.currency != null ? list.currency : 'PHP'}}{{list.regular != null ? list.regular : 0}}/{{list.label != null ? list.label : 'No Label'}}</b><i @click="$router.push('/add-rooms/'+ list.id)" class="fa fa-pencil ml-2"></i></span>
             </div>
             <p class="card-text">{{list.description !== null ? list.description : 'No description'}}</p>
-            <div class="row">
-              <!-- // lacking selectedFeature -->
-              <div class="col-md-6">
+            <div class="row" v-if="list.additional_info.add_ons.length > 0">
+              <div class="col-md-6" v-for="(item, index) in list.additional_info.add_ons" :key="index">
                 <span><i class="fa fa-check"></i></span>
-                <span>1 Queen Bed</span>
-              </div>
-              <div class="col-md-6">
-                <span><i class="fa fa-check"></i></span>
-                <span>In-room Safety Deposit Box</span>
+                <span>{{item}}</span>
               </div>
             </div>
-            <div class="row">
-              <!-- // lacking of addOns -->
-              <div class="col-md-6">
-                <span><i class="fa fa-check"></i></span>
-                <span>Bath Tub</span>
-              </div>
-              <div class="col-md-6">
-                <span><i class="fa fa-check"></i></span>
-                <span>Comfort Room</span>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6">
-                <span><i class="fa fa-check"></i></span>
-                <span>High-speed internet access</span>
-              </div>
-            </div>
-            <div class="mt-3" v-if="actionBtn===true">
-              <button class="btn btn-primary" style="height: 50px; width:150px">Bookings</button>
-              <button class="btn btn-secondary" style="height: 50px; width:150px">Details</button>
+            <div class="mt-1" v-if="actionBtn===true">
+              <button @click="$router.push('/own-bookings/' + list.title)" class="btn btn-primary" style="height: 50px; width:150px">Bookings</button>
+              <button @click="$router.push('/add-rooms/'+ list.id)" class="btn btn-secondary" style="height: 50px; width:150px">Details</button>
             </div>
           </div>
         </div>
@@ -54,8 +30,19 @@
 </template>
 
 <script>
+import CONFIG from 'src/config.js'
 export default {
-  props: ['actionBtn', 'list']
+  props: ['actionBtn', 'list'],
+  data(){
+    return {
+      config: CONFIG
+    }
+  },
+  methods: {
+    edit(){
+      this.$emit('isUpdate', this.list)
+    }
+  }
 }
 </script>
 
