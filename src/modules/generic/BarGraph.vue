5102bb676e945bd2368893cdc6b189e175f8bc62
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-11-15 17:00:48 +0800

    graph

---
+++ src/modules/generic/BarGraph.vue
@@ -0,0 +1,26 @@
+<script>
+import AUTH from 'src/services/auth'
+import { Line } from 'vue-chartjs'
+export default {
+  extends: Line,
+  props: ['data'],
+  components: {
+    // Line
+  },
+  mounted() {
+    let label = this.data.labels
+    let data = this.data.datasets
+    this.renderChart({
+      labels: label,
+      datasets: data
+    }, {
+      maintainAspectRatio: false
+    })
+  },
+  data(){
+    return {
+      user: AUTH.user
+    }
+  }
+}
+</script>
