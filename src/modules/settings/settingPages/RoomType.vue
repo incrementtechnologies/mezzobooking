fa994d90a8763ca81c2f138d2faa9395b28b9efa
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-03-29 11:01:45 +0800

    updates

--- src/modules/settings/settingPages/RoomType.vue
+++ src/modules/settings/settingPages/RoomType.vue
@@ -30,7 +30,7 @@
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
-            <h4 class="modal-title">Error Message</h4>
+            <h4 class="modal-title">Error</h4>
             <button type="button" class="close" data-dismiss="modal">&times;</button>
           </div>
           <div class="modal-body">
