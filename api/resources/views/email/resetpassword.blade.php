8404da0792766a4d8fac6f414fc67d29120754fb
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-07-08 09:48:48 +0800

    updates

--- api/resources/views/email/resetpassword.blade.php
+++ api/resources/views/email/resetpassword.blade.php
@@ -21,7 +21,7 @@
 </span>
 @if($user->account_type === 'ADMIN')
     <span class="text">
-        If you did not make this change, please <a href="{{env('APP_FRONT_END_URL_ADMIN')}}#/reset_password/{{$user->username}}/{{$user->code}}">reset</a> your password to secure your account and reply to this message to notify us.
+        If you did not make this change, please <a href="{{env('APP_FRONT_END_URL_ADMIN')}}/reset_password/{{$user->username}}/{{$user->code}}">reset</a> your password to secure your account and reply to this message to notify us.
     </span>
 @else
     <span class="text">
