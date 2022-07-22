52fc991c9b3cda3ba839c40bafd1959ded3af85d
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-07-22 16:58:19 +0800

    updates

--- api/app/Http/Controllers/EmailController.php
+++ api/app/Http/Controllers/EmailController.php
@@ -276,7 +276,7 @@ class EmailController extends APIController
 
     public function sendMyBookingUpdate($params){
         $user = $this->retrieveAccountDetails($params['account_id']);
-        if($use !== null){
+        if($user !== null){
             $params['from_email'] = $user['email'];
             $subject = 'Reservation Code: '.$params['code'] .'-'. $params['status'];
             Mail::to(env('MAIL_TO_ADDRESS'))->send(new MyBookingUpdate($params, $subject));
