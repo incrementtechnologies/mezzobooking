c102aef46d6214452fd23df0b5074eb30846ccf3
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-08-17 10:36:39 +0800

    updates

--- src/modules/calendar/index.vue
+++ src/modules/calendar/index.vue
@@ -1,13 +1,30 @@
 <template>
-  <vc-calendar is-expanded />
+  <vc-calendar :is-expanded="$screens({lg: true })" :attributes="attrs">
+
+  </vc-calendar>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+			attrs:[
+				{
+					highlight: {
+						// color: 'purple',
+						// fillMode: 'solid',
+						// contentClass: 'italic',
+						style: {
+							backgroundColor: 'brown'
+						}
+					},
+          dates: new Date(),
+				}
+			]
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
