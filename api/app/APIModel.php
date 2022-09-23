35f27a458cbf86af9188f88521e6d6fb68382c2c
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-09-23 13:36:36 +0800

    updates

--- api/app/APIModel.php
+++ api/app/APIModel.php
@@ -12,19 +12,9 @@ class APIModel extends Model
     use SoftDeletes;
     public $parent = [];
 
-    // public function children($childName){
-    //   $joinedTable = $this;
-    //   foreach($this->children as $childName => $child){
-    //     $joinedTable = $joinedTable
-    //   }
-    //   return $joinedTable;
-    // }
     public function getTableColumns() {
         return $this->getConnection()->getSchemaBuilder()->getColumnListing($this->getTable());
     }
-    // public function getTableDetail(){
-    //   return $this->getConnection()->getSchemaBuilder()->listTableDetails("inventory");
-    // }
     public function newModel($table, $attribute){
       $modelName = "App\\".str_replace(' ', '', ucwords(str_replace('_', ' ', str_singular($table))));
       return new $modelName($attribute);
