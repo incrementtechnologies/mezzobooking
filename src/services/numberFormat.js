d737768702f171394d4340ddea9dec652db449fe
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-09-28 13:43:54 +0800

    updates

--- src/services/numberFormat.js
+++ src/services/numberFormat.js
@@ -2,6 +2,11 @@ export default{
   format(number){
     let temp = number.toString().split('.')
     temp[0] = temp[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-    return temp.join('.')
+    let wholeNumber = temp.join('.')
+    if(wholeNumber.indexOf('.') === -1){
+      return wholeNumber + '.00'
+    }else{
+      return wholeNumber
+    }
   }
 }
